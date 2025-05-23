0efdd1915128e98fe18f2f9ece02efb34367000d
Wei Zhang <awzhan@amazon.com>
2025-05-22 22:31:38 -0700

    added start

---
+++ src/main/java/com/awzhan/game/sheep/view/Start.java
@@ -0,0 +1,19 @@
+package com.awzhan.game.sheep.view;
+
+import javax.swing.JFrame;
+
+public class Start extends JFrame {
+
+    public Start() {
+        this.setTitle("Sheep Game");
+        this.setSize(400, 800);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Start();
+    }
+}
