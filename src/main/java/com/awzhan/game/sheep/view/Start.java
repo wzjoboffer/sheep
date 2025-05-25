f52231877d066046f4a110b3d4fa5c444098c48e
Wei Zhang <awzhan@amazon.com>
2025-05-24 20:37:39 -0700

    added auto refresh

--- src/main/java/com/awzhan/game/sheep/view/Start.java
+++ src/main/java/com/awzhan/game/sheep/view/Start.java
@@ -10,6 +10,21 @@ public class Start extends JFrame {
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
+        
+        autoRefresh();
+    }
+
+    private void autoRefresh() {
+        new Thread(() -> {
+            while (true) {
+                repaint();
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    // ignore
+                }
+            }
+        }).start();
     }
 
     public static void main(String[] args) {
