36c3315822401d7f2dffabffaf8438a266181eff
Wei Zhang <awzhan@amazon.com>
2025-05-24 21:25:48 -0700

    updated Layer

--- src/main/java/com/awzhan/game/sheep/model/Layer.java
+++ src/main/java/com/awzhan/game/sheep/model/Layer.java
@@ -1,14 +1,20 @@
 package com.awzhan.game.sheep.model;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class Layer {
-    private Cell[][] matrix;
+    private final int rowNum;
+    private final int colNum;
+    private final int capacity;
+    private int size;
+    private final Cell[][] matrix;
 
-    public Layer(int x, int y) {
-        this.matrix = new Cell[x][y];
+    public Layer(int rowNum, int colNum) {
+        this.rowNum = rowNum;
+        this.colNum = colNum;
+        this.capacity = rowNum * colNum;
+        this.size = 0;
+        this.matrix = new Cell[rowNum][colNum];
     }
 }
