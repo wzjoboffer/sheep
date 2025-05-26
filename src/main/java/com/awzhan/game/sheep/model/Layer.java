0a451a38f8ad1c1efbb0030e223aefba0d746af2
Wei Zhang <awzhan@amazon.com>
2025-05-25 22:30:48 -0700

    added LayerUtil

--- src/main/java/com/awzhan/game/sheep/model/Layer.java
+++ src/main/java/com/awzhan/game/sheep/model/Layer.java
@@ -21,4 +21,15 @@ public class Layer {
         this.size = 0;
         this.matrix = new Cell[rowNum][colNum];
     }
+
+    public void printMatrix() {
+        for (int row = 0; row < matrix.length; row++) {
+            System.out.print("| ");
+            for (int col = 0; col < matrix[row].length; col++) {
+                final Brand brand = matrix[row][col].getBrand();
+                System.out.print(brand + " | ");
+            }
+            System.out.println();
+        }
+    }
 }
--- src/main/java/com/awzhan/game/sheep/util/LayerUtils.java
+++ src/main/java/com/awzhan/game/sheep/util/LayerUtils.java
@@ -1,7 +1,30 @@
 package com.awzhan.game.sheep.util;
 
+import com.awzhan.game.sheep.model.Brand;
+import com.awzhan.game.sheep.model.Cell;
+import com.awzhan.game.sheep.model.Layer;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class LayerUtils {
+
+    public static Layer buildLayer(final int rowNum, final int colNum) {
+        final Layer layer = new Layer(rowNum, colNum);
+        final Cell[][] matrix = layer.getMatrix();
+        final Brand[] brands = BrandUtils.buildBrands(layer.getCapacity());
+
+        int i = 0;
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                final Brand brand = brands[i++];
+                matrix[row][col] = new Cell(brand);
+            }
+        }
+        return layer;
+    }
+
+    public static void main(String[] args) {
+        final Layer layer = buildLayer(3, 5);
+        layer.printMatrix();
+    }
 }
